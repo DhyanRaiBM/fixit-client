0d6387ca0b7d6ef68bc99f35d1fb788fd7408e65
Dhyan Rai <dhyanraibm@gmail.com>
2024-08-29 21:44:56 +0530

    Final commit

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,8 @@ export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
       <h1 className="text-4xl font-bold text-gray-800 max-w-[60vw] text-center">
-        I haven't implemented this route as it's not part of the assessment.
+        I haven&rsquo;t implemented this route as it&rsquo;s not part of the
+        assessment.
       </h1>
       <p className="mt-4 text-lg text-gray-600">Page Not Found</p>
       <a href="/" className="mt-6 text-blue-500 hover:underline">
