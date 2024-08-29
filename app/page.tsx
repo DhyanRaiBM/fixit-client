44a16b7240a522090bad881f7d52e61b15af5611
Dhyan Rai <dhyanraibm@gmail.com>
2024-08-29 21:31:55 +0530

    Final commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,214 @@
-import Image from "next/image";
+'use client'
+import SearchBar from '@/components/SearchBar'
+import { UserButton, useUser } from '@clerk/nextjs'
+import React, { useEffect, useState } from 'react'
 
-export default function Home() {
+const Homepage = () => {
+  const text1 = 'Conversational Intelligence for Sales Optimization'
+  const text2 = 'Enabling Human Intelligence with Artificial Intelligence'
+  const [answer, setAnswer] = useState('')
+
+  const [displayedText, setDisplayedText] = useState('')
+
+  useEffect(() => {
+    let words = answer.split(' ')
+    let index = 0
+
+    const intervalId = setInterval(() => {
+      if (index < words.length) {
+        setDisplayedText((prev) => prev + (index > 0 ? ' ' : '') + words[index])
+        index++
+      } else {
+        clearInterval(intervalId)
+      }
+    }, 100) // Adjust the speed here (in milliseconds)
+
+    return () => setDisplayedText('')
+  }, [answer])
+
+  let ind = 0
+
+  const [isScrolled, setIsScrolled] = useState(false)
+  const { isLoaded, user } = useUser()
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+  }, [])
+
+  useEffect(() => {
+    if (isLoaded) {
+      if (user) {
+        document.body.classList.remove('no-scroll')
+      } else {
+        document.body.classList.add('no-scroll')
+      }
+    }
+  }, [isLoaded, user])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <header
+        className={`fixed top-0 z-50 w-full left-0 right-0 transition-all ease duration-200 py-0 h-[60px] bg-white bg-transparent ${
+          isScrolled ? 'bg-white text-black' : 'bg text-white'
+        } block`}
+      >
+        <nav className="relative w-full mx-auto h-full flex items-center justify-between">
+          <div className="flex items-center justify-between w-[95%] sm:w-[85%] mx-auto">
+            <a href="/">
+              <div className="flex items-center transition-all ease duration-200 gap-x-2">
+                <img
+                  alt="fixit"
+                  loading="lazy"
+                  width="25"
+                  height="25"
+                  src="/fixit.svg"
+                  className="text-transparent"
+                />
+                <h4
+                  className={`text-[2rem] text-[#263238] ${
+                    isScrolled ? ' text-black' : ' text-white'
+                  }`}
+                >
+                  fiXit
+                </h4>
+              </div>
+            </a>
+
+            <div className="flex text-sm lg:w-80 max-w-80 justify-end items-center gap-x-5">
+              <a href="/about">
+                <button
+                  className={`bg-transparent rounded-sm border px-3 py-1.5 flex min-w-fit font-bold text-main border-main ${
+                    isScrolled && 'border-1 border-black'
+                  }`}
+                >
+                  About Us
+                </button>
+              </a>
+              {!isLoaded ? (
+                <p>Loading...</p>
+              ) : !user ? (
+                <a href="/sign-in">
+                  <button
+                    className={`bg-transparent rounded-sm border px-3 py-1.5 flex w-full min-w-max font-bold text-main border-main ${
+                      isScrolled && 'border-1 border-black'
+                    }`}
+                  >
+                    Login
+                  </button>
+                </a>
+              ) : (
+                <UserButton />
+              )}
+            </div>
+          </div>
+        </nav>
+      </header>
+
+      {/* Main Content */}
+      <main className="">
+        <div className="bg-black">
+          <div className="bg-new-color xl:px-[160px] max-sm:px-5">
+            <div className="text-main w-full text-4xl pt-[8rem] pb-20 lg:pb-10 max-sm:pb-0 xl:pt-[2rem] xl:h-screen flex flex-col xl:flex-row items-center justify-center xl:justify-center">
+              <div className="flex  xl:ml-10 pt-[5rem] xl:pt-0 h-fit xl:w-[60%] pb-0 flex-col text-center text-white items-center xl:items-start gap-4 sm:-mt-[2%] justify-center xl:justify-between ">
+                <h3 className="w-full  leading-tight text-[45px] sm:text-[70px] text-center md:text-[70px] xl:text-[50px] md:mt-5 p-0 xl:text-start font-proximaBold -mt-[2%] bg-clip-text text-gray-700 width-[60%]">
+                  {text1.split('').map((char, index) => (
+                    <>
+                      <span
+                        key={index}
+                        className="text-reveal font-bold"
+                        style={{ animationDelay: `${ind++ * 0.05}s` }}
+                      >
+                        {char === ' ' ? '\u00A0' : char}
+                      </span>
+                      {index === 14 && <br />}
+                      {index === 31 && <br />}
+                    </>
+                  ))}
+                </h3>
+                <h3 className="w-full leading-tight text-[30px] sm:text-[30px] text-center md:text-[30px] xl:text-[35px] md:mt-5 p-0 xl:text-start font-proximaBold -mt-[2%] bg-clip-text text-gray-700">
+                  {text2.split('').map((char, index) => (
+                    <>
+                      <span
+                        key={index}
+                        className="text-reveal font-bold"
+                        style={{ animationDelay: `${ind++ * 0.05}s` }}
+                      >
+                        {char === ' ' ? '\u00A0' : char}
+                      </span>
+                      {/* {index === 26 && <br />} */}
+                    </>
+                  ))}
+                </h3>
+                <div
+                  id="calendly-inline-widget"
+                  className="fixed right-0 bottom-0"
+                  data-auto-load="false"
+                  style={{ minWidth: '320px', height: '580px' }}
+                ></div>
+                <div className="flex flex-row gap-12 md:gap-24">
+                  <div>
+                    <button className="text-lg font-bold mt-12 animate-in slide-in-from-bottom-14 duration-700 text-[#e46e26] hover:text-[#EDC0C0] rounded-sm underline">
+                      <a href="/sign-in">Try now &nbsp;&nbsp;&gt;</a>
+                    </button>
+                  </div>
+                  <div>
+                    <button className="text-lg font-bold mt-12 animate-in slide-in-from-bottom-14 duration-700 text-[#e46e26] hover:text-[#EDC0C0] rounded-sm underline">
+                      <a href="/sign-in">Schedule now &nbsp;&nbsp;&gt;</a>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="lg:w-[50%] xl:w-[50%] xl:mr-10 h-screen mb-0 xl:mb-0 border-gray-600 rounded-2xl flex flex-col justify-center items-center">
+                <div className="flex flex-col  items-center rounded-2xl px-8 lg:py-10 bg-transparent bg-record cursor-pointer">
+                  <div className="text-white font-bold text-2xl">
+                    Give it a try.
+                  </div>
+                  <div className="text-white text-base text-center xl:px-10">
+                    Click the mic to transcribe live in English or select
+                    another language.
+                  </div>
+                </div>
+                <div className="text-white bg-[#0a0a0a] rounded-bl-2xl rounded-br-2xl mt-3 p-4 w-full text-base">
+                  <span className="text-xl font-bold">Transcription</span>
+                  <div className="bg-[black] px-4 py-2 mt-3 rounded-lg">
+                    Click the mic to transcribe live in English or select
+                    another language.
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full pt-5 mb-[150px] mt-[30px]  max-sm:py-5 flex bg-new-color flex-col items-center justify-center">
+              <div className="w-full h-full bg-transparent flex flex-col justify-center items-center">
+                <div className="w-full">
+                  <div className="w-full px-4 max-sm:px-0 sm:h-3/5 bg-transparent flex flex-col items-center m-auto">
+                    <div className="w-full h-10 rounded-t-2xl flex items-center justify-center">
+                      <span className="text-neutral-400 text-center leading-[3.45rem] px-6 max-sm:px-2 font-bold font-proximaBold text-[45px] max-sm:text-2xl py-6 max-sm:py-2 m-auto">
+                        Enabling sales teams in leading companies
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <SearchBar setAnswer={setAnswer} />
+            </div>
+            <div className="mb-[100px]">
+              {answer && (
+                <div className="bg-gray-100 text-gray-800 p-4 rounded-lg shadow-md max-w-3xl mx-auto">
+                  <p className="whitespace-pre-line">{displayedText}</p>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      </main>
+    </div>
+  )
 }
+
+export default Homepage
---
+++ components/SearchBar.tsx
@@ -0,0 +1,80 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { TbSquareRoundedArrowRightFilled } from 'react-icons/tb'
+
+interface Props {
+  setAnswer: React.Dispatch<React.SetStateAction<string>>
+}
+
+const SearchBar = ({ setAnswer }: Props) => {
+  const [question, setQuestion] = useState('')
+
+  function onSubmit() {
+    const data = {
+      text: question.toLowerCase(),
+    }
+
+    axios
+      .post(process.env.NEXT_PUBLIC_URL as string, data)
+      .then((response) => {
+        if (response.status !== 404) {
+          setAnswer(response.data.answer)
+        } else {
+          setAnswer('Answer not found, please try something else.')
+        }
+      })
+      .catch((error) => {
+        setAnswer('Answer not found, please try something else.')
+        console.error('Error:', error)
+      })
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-[50vh] ">
+      {/* Logo */}
+      <div className="mb-6 w-[200px] flex justify-center items-center gap-5">
+        <img src="/fixit.svg" alt="fiXit Logo" className="w-10 h-10 inline" />
+        <span className="text-4xl font-semibold">fiXit</span>
+      </div>
+
+      {/* Search Bar */}
+      <div className="relative w-3/5 sm:min-w-[60vw] mb-6 ">
+        <input
+          type="text"
+          value={question}
+          placeholder="SalesGPT can help you to browse uncovered topics"
+          className="w-full p-4 pl-4 pr-10 text-gray-700 bg-white border-2 rounded-lg shadow-sm focus:outline-none focus:border-blue-400"
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+        <button
+          className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+          onClick={onSubmit}
+        >
+          <TbSquareRoundedArrowRightFilled className="w-7 h-7" />
+        </button>
+      </div>
+
+      {/* Suggested Topics */}
+      <div className="flex flex-wrap justify-center space-x-2">
+        <span>Try Asking</span>
+        {[
+          'Budget analysis overview',
+          'Tax proposals summary',
+          'Expenditure breakdown',
+          'Disinvestment targets explained',
+          'Focus on fiscal responsibility',
+        ].map((topic, index) => (
+          <span
+            key={index}
+            className="px-4 py-2 mb-2 text-sm text-gray-700 bg-gray-200 rounded-lg cursor-pointer hover:bg-blue-100"
+            onClick={() => setQuestion(topic)}
+          >
+            {topic}
+          </span>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar
